1835a37ac8c16925b9a18c6246af4e40588d7d91
Sergio <sergiot.fontal@gmail.com>
2023-02-25 20:22:14 +0100

    Create startServer function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 import "./loadEnvirontment.js";
+import { startServer } from "./server/routers/startServer.js";
+
+await startServer(+process.env.PORT!);
---
+++ src/server/routers/startServer.ts
@@ -0,0 +1,31 @@
+import { app } from "../index.js";
+import createDebug from "debug";
+import type { CustomError } from "../../CustomError/CustomError.js";
+
+const debug = createDebug("app:startServer");
+export const startServer = async (port: number) =>
+  new Promise((resolve) => {
+    const server = app.listen(port, () => {
+      debug(
+        `${"▄".repeat(60)}\n█${" ".repeat(
+          58
+        )}█\n█        Server is listening requests on port ${port}         █\n█${" ".repeat(
+          58
+        )}█\n${"▀".repeat(60)}`
+      );
+
+      resolve(server);
+    });
+
+    server.on("error", (error: CustomError) => {
+      if (error.code === "EADDRINUSE") {
+        debug(
+          `${"▄".repeat(60)}\n█${" ".repeat(
+            58
+          )}█\n█               Port ${port} is already in use                █\n█${" ".repeat(
+            58
+          )}█\n${"▀".repeat(60)}`
+        );
+      }
+    });
+  });
