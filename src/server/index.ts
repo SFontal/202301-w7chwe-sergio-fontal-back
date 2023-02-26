226ce9074423726adcee91c40d926dd7c81108ec
Sergio <sergiot.fontal@gmail.com>
2023-02-26 15:50:15 +0100

    Create usersRouter and a route to getUsers controller

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import helmet from "helmet";
+import usersRouter from "./routers/usersRouter.js";
 
 export const app = express();
 
 app.use(helmet());
+
+app.use("/users", usersRouter);
